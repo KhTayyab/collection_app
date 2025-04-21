6f844311acb5141706ee3d88b4e682305766206d
Khawaja Tayyab <khawaja.tayyab@alchemative.com>
2025-04-21 11:03:54 +0500

    secret keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "Simply Collection Image"
-  config.api_key = "acdce4f440e4777c848fe94edb2157fd"
-  config.secret = "bb633595945eb58be9fd813d6cbd01f8"
+  config.api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  config.secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   config.scope = "read_products, write_products, read_orders, write_orders"
   config.embedded_app = true
 end
