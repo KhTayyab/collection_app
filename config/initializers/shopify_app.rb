061659c2ea1b45d3df254d1962921ea597ef2989
Khawaja Tayyab <khawaja.tayyab@alchemative.com>
2025-04-21 10:48:14 +0500

    new one

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.application_name = "Simply Collection Image"
+  config.api_key = "acdce4f440e4777c848fe94edb2157fd"
+  config.secret = "bb633595945eb58be9fd813d6cbd01f8"
+  config.scope = "read_products, write_products, read_orders, write_orders"
+  config.embedded_app = true
+end
